bcb28695ee531e34c1ef2b49132ee1e2e9c77055
cpu-man <71815606+cpu-man@users.noreply.github.com>
2026-03-20 11:17:05 +0100

    et eller andet

---
+++ DesignPattern-Pizza/DesignPattern-Pizza/IObserver.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern_Pizza
+{
+    internal interface IObserver
+    {
+
+        void Update(IPizza ipizza)
+        {
+
+        }
+    }
+}
